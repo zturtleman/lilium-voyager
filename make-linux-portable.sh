bd494b08f38b3d384be06952a5ec6483c46af5b4
Zack Middleton <zack@cloemail.com>
2023-04-02 18:07:15 -0500

    Add SDL 2.24.0 libs for Linux
    Add the Linux SDL libs to the repo like Windows and macOS instead of
    downloading them when running make-linux-portable.sh.

--- make-linux-portable.sh
+++ make-linux-portable.sh
@@ -2,33 +2,10 @@
 ARCH=$1
 ERROR=0
 
-# Install directory for Spearmint SDL2 builds (i.e., /home/zack/Local/SDL-2.0.X).
-DIR=/home/zack/Local
-
-# SDL version to install and compile against.
-# See available versions at http://clover.moe/downloads/spearmint-make-linux-portable-deps/
-SDLVERSION=2.0.8
-
-#
-# Use make-linux-portable.local file to set DIR and (optionally) SDLVERSION
-#
-SETTINGS="$(dirname $0)/make-linux-portable.local"
-if [ -f "$SETTINGS" ] ; then
-	. "$SETTINGS" # source "$SETTINGS"
-
-	if [ -z "$DIR" ] || [ ! -d "$DIR" ]; then
-		echo "Error: DIR specified in '$SETTINGS' not found."
-		echo
-		ERROR=1
-	fi
-else
-	# Require settings file to exist but only error if specified arch
-	if [ -n "$ARCH" ]; then
-		echo "Error: You need to create '$SETTINGS', see usage."
-		echo
-		ERROR=1
-	fi
-fi
+# Adjust these for the project.
+CLIENTBIN=liliumvoyhm
+SERVERBIN=liliumvoyded
+RENDERER_PREFIX=liliumvoyhm_renderer_
 
 if [ -z "$ARCH" ] || [ $ERROR -ne 0 ]; then
 	echo "Usage: make-linux-portable.sh <arch> [make options]"
@@ -45,13 +22,7 @@ if [ -z "$ARCH" ] || [ $ERROR -ne 0 ]; then
 	echo
 	echo "DIRECTIONS"
 	echo
-	echo "1. Create a directory to store Spearmint SDL2 builds and create a file named"
-	echo "'$SETTINGS' containing:"
-	echo
-	echo "    # Example comment line"
-	echo "    DIR=/path/to/spearmint-make-linux-portable-deps"
-	echo
-	echo "2. To build the game clients (and download Spearmint SDL2 builds if needed) run"
+	echo "1. To build the game clients (and download Spearmint SDL2 builds if needed) run"
 	echo "the following commands:"
 	echo
 	echo "    make clean-release ARCH=x86"
@@ -62,7 +33,7 @@ if [ -z "$ARCH" ] || [ $ERROR -ne 0 ]; then
 	echo "Note: make clean-release ARCH=... is only needed if you previously compiled as"
 	echo "non-portable."
 	echo
-	echo "3. Make the following files are included relative to the game client in the"
+	echo "2. Make the following files are included relative to the game client in the"
 	echo "downloads:"
 	echo
 	echo "    ./lib/x86/libSDL2-2.0.so.0"
@@ -85,46 +56,10 @@ if [ -z "$ARCH" ] || [ $ERROR -ne 0 ]; then
 	exit $ERROR
 fi
 
-if [ ! -d "$DIR/SDL-$SDLVERSION" ] ; then
-	echo "Downloading Spearmint SDL-$SDLVERSION builds into '$DIR'..."
+SDL_LIBDIR="code/libs/linux-$ARCH"
 
-	# Download SDL2 libs and headers into $DIR.
-	if [ ! -f "$DIR/SDL-$SDLVERSION.tar.xz" ] ; then
-		# Yes, wget and curl use -o with different capitalization to set output filename.
-		if command -v wget >/dev/null 2>&1 ; then
-			wget -O "$DIR/SDL-$SDLVERSION.tar.xz" "http://clover.moe/downloads/spearmint-make-linux-portable-deps/SDL-$SDLVERSION.tar.xz"
-			ERROR=$?
-		elif command -v curl >/dev/null 2>&1 ; then
-			curl -o "$DIR/SDL-$SDLVERSION.tar.xz" "http://clover.moe/downloads/spearmint-make-linux-portable-deps/SDL-$SDLVERSION.tar.xz"
-			ERROR=$?
-		else
-			echo "Error: Need to install wget or curl to download Spearmint SDL2 builds."
-			exit 1
-		fi
-
-		if [ $ERROR -ne 0 ] ; then
-			echo "Error: Failed to download SDL-$SDLVERSION!"
-			exit 1
-		fi
-	fi
-
-	# Extract into $DIR/SDL-$SDLVERSION/.
-	tar xJf "$DIR/SDL-$SDLVERSION.tar.xz" -C "$DIR"
-
-	# Edit prefix in sdl2-config to point to the install directory.
-	sed -i -e "s|/home/zack/Local|$DIR|" "$DIR/SDL-$SDLVERSION/build-x86/bin/sdl2-config"
-	sed -i -e "s|/home/zack/Local|$DIR|" "$DIR/SDL-$SDLVERSION/build-x86_64/bin/sdl2-config"
-fi
-
-SDL_PREFIX="$DIR/SDL-$SDLVERSION/build-$ARCH"
-
-if [ ! -d "$SDL_PREFIX" ] ; then
-	echo "Arch '$ARCH' not found in '$DIR/SDL-$SDLVERSION'"
-	exit 1
-fi
-
-SDL_CFLAGS=$("$SDL_PREFIX/bin/sdl2-config" --cflags)
-SDL_LIBS=$("$SDL_PREFIX/bin/sdl2-config" --libs)
+SDL_CFLAGS="-Icode/SDL2/include -D_REENTRANT"
+SDL_LIBS="-L$SDL_LIBDIR -lSDL2"
 
 # Don't pass arch ($1) to make in $*
 shift 1
@@ -140,28 +75,28 @@ fi
 if [ ! -d build/release-linux-$ARCH/lib/$ARCH/ ] ; then
 	mkdir -p build/release-linux-$ARCH/lib/$ARCH/
 fi
-if [ ! -f build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0 ] || [ $(stat -c %Y build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0) != $(stat -c %Y "$SDL_PREFIX/lib/libSDL2-2.0.so.0") ] ; then
-	cp --preserve=timestamps "$SDL_PREFIX/lib/libSDL2-2.0.so.0" build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0
+if [ ! -f build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0 ] || [ $(stat -c %Y build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0) != $(stat -c %Y "$SDL_LIBDIR/libSDL2-2.0.so.0") ] ; then
+	cp --preserve=timestamps "$SDL_LIBDIR/libSDL2-2.0.so.0" build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0
 fi
 
 # It's interesting to see why a glibc version is required.
 # Another interesting thing is "ldd -v filename" to see what libaries are requires.
-VERBOSE=0
+VERBOSE=1
 if [ $VERBOSE -eq 1 ] ; then
 	# Find glibc lines, remove text before GLIBC, then sort by required glibc version
 	# "sort -t _ -k 2 -V" is short form of "sort --field-separator='_' --key=2 --version-sort"
 	echo
-	echo "liliumvoyded.$ARCH:"
-	objdump -T build/release-linux-$ARCH/liliumvoyded.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
+	echo "$SERVERBIN.$ARCH:"
+	objdump -T build/release-linux-$ARCH/$SERVERBIN.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
 	echo
-	echo "liliumvoyhm.$ARCH:"
-	objdump -T build/release-linux-$ARCH/liliumvoyhm.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
+	echo "$CLIENTBIN.$ARCH:"
+	objdump -T build/release-linux-$ARCH/$CLIENTBIN.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
 	echo
-	echo "liliumvoyhm_renderer_opengl1_$ARCH.so:"
-	objdump -T build/release-linux-$ARCH/liliumvoyhm_renderer_opengl1_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
+	echo "${RENDERER_PREFIX}opengl1_$ARCH.so:"
+	objdump -T build/release-linux-$ARCH/${RENDERER_PREFIX}opengl1_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
 	echo
-	echo "liliumvoyhm_renderer_opengl2_$ARCH.so:"
-	objdump -T build/release-linux-$ARCH/liliumvoyhm_renderer_opengl2_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
+	echo "${RENDERER_PREFIX}opengl2_$ARCH.so:"
+	objdump -T build/release-linux-$ARCH/${RENDERER_PREFIX}opengl2_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
 	echo
 	echo "lib/$ARCH/libSDL2-2.0.so.0:"
 	objdump -T build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0 | grep GLIBC | sed -e "s/.*GLIBC_/GLIBC_/g" | sort -t _ -k 2 -V
@@ -169,10 +104,10 @@ fi
 
 # Find glibc lines, remove text before and including GLIBC_, remove trailing function name, sort by required glibc version, then grab the last (highest) version
 # "sort -uV" is short form of "sort --unique --version-sort"
-GLIBC_SERVER=$(objdump -T build/release-linux-$ARCH/liliumvoyded.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
-GLIBC_C1=$(objdump -T build/release-linux-$ARCH/liliumvoyhm.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
-GLIBC_C2=$(objdump -T build/release-linux-$ARCH/liliumvoyhm_renderer_opengl1_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
-GLIBC_C3=$(objdump -T build/release-linux-$ARCH/liliumvoyhm_renderer_opengl2_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
+GLIBC_SERVER=$(objdump -T build/release-linux-$ARCH/$SERVERBIN.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
+GLIBC_C1=$(objdump -T build/release-linux-$ARCH/$CLIENTBIN.$ARCH | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
+GLIBC_C2=$(objdump -T build/release-linux-$ARCH/${RENDERER_PREFIX}opengl1_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
+GLIBC_C3=$(objdump -T build/release-linux-$ARCH/${RENDERER_PREFIX}opengl2_$ARCH.so | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
 GLIBC_C4=$(objdump -T build/release-linux-$ARCH/lib/$ARCH/libSDL2-2.0.so.0 | grep GLIBC | sed -e "s/.*GLIBC_//g" | cut -d' ' -f1 | sort -uV | tail -1)
 GLIBC_CLIENT=$(printf "$GLIBC_C1\n$GLIBC_C2\n$GLIBC_C3\n$GLIBC_C4\n" | sort -uV | tail -1)
 
